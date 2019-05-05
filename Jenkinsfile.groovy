ae6e32dbe58f4ae597db062568315c728e2f2629
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-05-04 20:23:57 -0500

    change

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,7 +2,7 @@ node{
     properties([parameters([string(defaultValue: '', description: 'please input IP', name: "ENV", trim: false)])])
     git "https://github.com/dilfuza97/Terraform-aws-K8_modules.git"
     stage("Install Terraform"){
-        sh "sudo yum install wget unzip zip -y"
+        sh "yum install wget unzip zip -y"
         sh "wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip"
         sh "unzip terraform_0.11.13_linux_amd64.zip"
         sh "mv terraform /bin/"
