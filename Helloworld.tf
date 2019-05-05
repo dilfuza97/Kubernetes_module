f272511bfaf993ec64981cdaa91a02a71f193494
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-05-05 14:32:47 -0500

    Kubernetes pod

---
+++ Helloworld.tf
@@ -0,0 +1,30 @@
+resource "kubernetes_pod" "hello_world" {
+  metadata {
+    name = "hello world"
+  }
+
+  spec {
+    container {
+      name  = "hello-world"
+      image = "hello-world"
+    }
+  }
+}
+
+resource "kubernetes_service" "hello_world" {
+  metadata {
+    name = "hello-world"
+  }
+
+  spec {
+    port {
+      protocol    = "TCP"
+      port        = 80
+      target_port = "80"
+    }
+
+    selector {
+      app = "hw"
+    }
+  }
+}
