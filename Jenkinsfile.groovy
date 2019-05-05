c97f18769361998aa9002d7535f33f9d6a4f7a32
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-05-04 20:30:13 -0500

    added ssh

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,16 +1,16 @@
 node{
-    // properties([parameters([string(defaultValue: '', description: 'please input IP', name: "ENV", trim: false)])])
+    properties([parameters([string(defaultValue: '', description: 'please input IP', name: "ENV", trim: false)])])
     git "https://github.com/dilfuza97/Terraform-aws-K8_modules.git"
-    stage("Install Terraform"){
-        sh "yum install wget unzip zip -y"
-        sh "wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip"
-        sh "unzip terraform_0.11.13_linux_amd64.zip"
-        sh "mv terraform /bin/"
+    // stage("Install Terraform"){
+    //     sh "yum install wget unzip zip -y"
+    //     sh "wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip"
+    //     sh "unzip terraform_0.11.13_linux_amd64.zip"
+    //     sh "mv terraform /bin/"
     }
     stage("Terraform init"){
-        sh "terraform init"
-        sh "terraform plan"
-        sh "terraform apply"
+        sh "ssh ec2-user@${ENV} terraform init"
+        sh "ssh ec2-user@${ENV} terraform plan"
+        sh "ssh ec2-user@${ENV} terraform apply"
     }     
 }      
         
