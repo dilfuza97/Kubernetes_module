5ab6bdeb3313e960172929c63a9ab3a6050b3235
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-05-04 20:52:41 -0500

    change

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,6 @@
 node{
     properties([parameters([string(defaultValue: '', description: 'please input IP', name: "ENV", trim: false)])])
-    git "https://github.com/dilfuza97/Terraform-aws-K8_modules.git"
+    // git "https://github.com/dilfuza97/Terraform-aws-K8_modules.git"
     // stage("Install Terraform"){
     //     sh "yum install wget unzip zip -y"
     //     sh "wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip"
@@ -8,12 +8,12 @@ node{
     //     sh "mv terraform /bin/"
     }
     stage("Copy tf files"){
-        sh "scp -r * ec2-user@${ENV}:/tmp/"
+        sh "ssh ec2-user@${ENV} git clone https://github.com/dilfuza97/Terraform-aws-K8_modules.git"
     }    
     stage("Terraform init"){
-        sh "ssh ec2-user@${ENV} cd /tmp/ && terraform init"
-        sh "ssh ec2-user@${ENV} cd /tmp/ && terraform plan"
-        sh "ssh ec2-user@${ENV} cd /tmp/ && terraform apply"
+        sh "ssh ec2-user@${ENV} cd Terraform-aws-K8_modules && terraform init"
+        sh "ssh ec2-user@${ENV} cd Terraform-aws-K8_modules && terraform plan"
+        sh "ssh ec2-user@${ENV} cd Terraform-aws-K8_modules && terraform apply"
     } 
     
       
