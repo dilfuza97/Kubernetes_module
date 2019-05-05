735d2c1799a814510a95c1ba6261cc526b426ec2
root <root@ip-172-31-29-4.eu-west-1.compute.internal>
2019-05-05 18:36:16 +0000

    change

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-december-dilfuza2"
+    bucket = "terrafor-january-dilfuza2"
     key    = "infra.state"
-    region = "eu-west-1"
+    region = "us-east-1"
   }
 }
