4a0964cebaa5af37a14d62d39237d0b6fe6f84f0
Dilfuza Ibaidullaeva <47833854+dilfuza97@users.noreply.github.com>
2019-05-09 18:06:23 -0500

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,6 +5,6 @@ node{
 
   stage("Terraform"){
     sh "terraform init"
-    sh "terraform destroy -var-file=dev.tfvars --auto-approve"
+    sh "terraform apply -var-file=dev.tfvars --auto-approve"
   }
 }
