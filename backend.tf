739f41f6b28cbba2490b876c5adf1b375e753630
root <root@ip-172-31-39-149.eu-west-1.compute.internal>
2019-05-14 19:00:07 +0000

    create

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "terrafor-january-dilfuza2"
+    bucket = "dilfuza-terraform"
     key    = "infra.state"
-    region = "us-east-1"
+    region = "eu-west-1"
   }
 }
