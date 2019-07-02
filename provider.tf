4ada66f911d55376bed23883f9f93a54ce16d321
Dilfuza Ibaidullaeva <47833854+dilfuza97@users.noreply.github.com>
2019-07-02 18:14:48 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region     = "us-west-1"
-  access_key = "AKIAVE4EALEVRQKS44OQ"
-  secret_key = "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"
+  access_key = "----"
+  secret_key = "---"
 }
 
